7fa854e037f42ddeac95fe6f29a6bd2f8ef7314f
CJROW1 <jrheault123@gmail.com>
2025-08-06 15:48:15 -0500

    Update FuelCar.java

--- FuelCar.java
+++ FuelCar.java
@@ -16,15 +16,75 @@ public class FuelCar implements iTrainCar {
     private int currentFuelLevel;
     private iTrainCar next;
 
+    //creates a new fuel car with a given max fuel amount
     public FuelCar(int maxFuelLevel) {
-
+        if(maxFuelLevel <= 0 ){
+            throw new IllegalArgumentException();
+        }
+        this.maxFuelLevel = maxFuelLevel;
+        currentFuelLevel = 0;
     }
 
+    //refuel the tank up to the max fuel level if given too much return false
     public boolean refuel(int amount) {
-
+        boolean refueled = false;
+        if(amount <= 0){
+            throw new IllegalArgumentException();
+        }
+        //if the amount exceeds the max return false
+        if(currentFuelLevel + amount > maxFuelLevel){
+            refueled = false;
+        }else{
+            currentFuelLevel += amount;
+            refueled = true;
+        }
+        return refueled;
     }
 
+    //consumes a given amount of fuel from the curretn amount of fuel 
     public boolean consumeFuel(int amount) {
-
+        boolean consumed = false;
+        if(amount <= 0){
+            throw new IllegalArgumentException();
+        }
+        //check if tehre is aenough fule and consume that amount 
+        if(currentFuelLevel - amount >= 0){
+            currentFuelLevel -= amount;
+            consumed = true;
+        }
+        return consumed;
+    }
+    //return the type of the car
+    public String getType(){
+        return FUEL;
+    }
+    //get the maximum capacity of the fuel car
+    public int getMaxCapacity() {
+        return maxFuelLevel;
+    }
+    //get the next car in the train
+    public iTrainCar getNext(){
+        return next;
+    }
+    //set the next car in the train
+    public void setNext(iTrainCar next){
+        this.next = next;
+    }
+    //the contents of the car represented by a string 
+    public String toStringContents(){
+        String contents = "Fuel: [";
+        for(int i = 0; i < maxFuelLevel; i++){
+            if(i < currentFuelLevel){
+                contents += "#";
+            }else{
+                contents += " ";
+            }
+        }
+        contents += "]";
+        return contents;
+    }
+    //creates a to string for the fuel car showing how much it has and its max
+    public String toString(){
+        return String.format("Fuel car (%d/%d)", currentFuelLevel, maxFuelLevel);
     }
 }
